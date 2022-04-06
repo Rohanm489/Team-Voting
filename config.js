0947d8a0e302acb465ff7807ea91de94c064008c
Rohanm489 <87101051+Rohanm489@users.noreply.github.com>
2022-04-06 18:42:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB9mjai2R_tz3ay83Nuvg5cD5G3tyL5s0c",
+  authDomain: "my-first-project-ab4f7.firebaseapp.com",
+  projectId: "my-first-project-ab4f7",
+  storageBucket: "my-first-project-ab4f7.appspot.com",
+  messagingSenderId: "961818700347",
+  appId: "1:961818700347:web:af0c8f7cae965953a9ca1e"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
